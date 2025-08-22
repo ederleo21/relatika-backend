555dccdf78d9b504570556c433fda52752f124fa
ederleo <ederordonez10@gmail.com>
2025-08-21 23:01:36 -0500

    Serializer de usuario modificado

--- apps/users/api/v1/serializers.py
+++ apps/users/api/v1/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from apps.users.models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
 
     class Meta:
         model = CustomUser
